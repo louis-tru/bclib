70902e1521e14a8438a633b043bd175b0f5721a1
louistru <louistru@hotmail.com>
2021-11-09 17:59:23 +0800

    fix api error

--- api/keys.ts
+++ api/keys.ts
@@ -20,7 +20,7 @@ export default class extends ApiController {
 		return keys.impl.keychain.addressList(this.userName);
 	}
 
-	address(part_key?: string) {
+	address({part_key}:{part_key?: string}) {
 		return keys.impl.keychain.address(this.userName, part_key);
 	}
 
