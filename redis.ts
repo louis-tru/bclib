ac3fc6c6fafb7498421c457ac57e3cb7f14a1baa
chuxuewen <louistru@hotmail.com>
2023-01-11 19:20:23 +0800

    update redis

--- redis.ts
+++ redis.ts
@@ -6,16 +6,16 @@
 import {createClient} from 'redis/dist/index';
 import cfg from './cfg';
 
-export const redis = createClient({url: cfg.redis || 'redis://127.0.0.1:6379/0'});
+export const client = createClient({url: cfg.redis || 'redis://127.0.0.1:6379/0'});
 
-export type Redis = typeof redis;
+export type Redis = typeof client;
 
 function getKey(key: string) {
-	return `__cache_${key}`;
+	return `${cfg.name}_${key}`;
 }
 
 export async function get<T = any>(key: string) {
-	var val = await redis.get(getKey(key));
+	var val = await client.get(getKey(key));
 	if (val && val != 'null') {
 		try {
 			var o = JSON.parse(val) as { __time__: number, val: T };
@@ -32,38 +32,45 @@ export async function get<T = any>(key: string) {
 export async function set(key: string, val: any, time?: number) {
 	time = Number(time) || 0;
 	var o = { __time__: time ? time + Date.now(): 0, val };
-	await redis.set(getKey(key), JSON.stringify(o));
+	await client.set(getKey(key), JSON.stringify(o));
 }
 
 export async function del(key: string) {
-	await redis.del(getKey(key));
+	await client.del(getKey(key));
 }
 
-async function test() {
+export async function test() {
 
-	await redis.set('a', 'A');
-	await redis.hSet('b', 'a', 100);
+	await client.set('a', 'A');
 
-	await redis.sendCommand(['HMSET', 'b', 'a', 'A', 'b', 'B', 'c', 'C', 'd', '']);
+	await client.sendCommand(['HMSET', 'b', 'a', 'A', 'b', 'B', 'c', 'C', 'd', '']);
 
-	var a = await redis.get('a');
-	var b = await redis.hGetAll('b');
-	var c = await redis.hGetAll('f');
+	let a = await client.get('a');
+	let b = await client.hGetAll('b');
+	let c = await client.hGetAll('c');
 
 	console.log(a, b, c);
+
+	await client.hSet('b', 'a', 100);
+
+	let b_a = await client.hGet('b', 'a');
+	let b2 = await client.hGetAll('b');
+
+	console.log(b_a, b2);
+
 }
 
 export async function fulushAll() {
-	await redis.sendCommand(['FLUSHALL']);
+	await client.sendCommand(['FLUSHALL']);
 }
 
 export async function initialize(isClean?: boolean) {
 
-	redis.on('error', (err:any) =>console.warn('Redis', err.message));
-	redis.on('connect', () =>console.log('Redis connect'));
-	redis.on('disconnect', () =>console.log('Redis disconnect'));
+	client.on('error', (err:any) =>console.warn('Redis', err.message));
+	client.on('connect', () =>console.log('Redis connect'));
+	client.on('disconnect', () =>console.log('Redis disconnect'));
 
-	await redis.connect();
+	await client.connect();
 
 	if (isClean) {
 		await fulushAll();
