b579ee597f7ddf21c0dab6e27f7ebb31e0e7a4c8
xuliang <xuliang@inmyshow.com>
2023-04-14 18:11:27 +0800

    merge from xuewen code fix web server path attack

--- server.ts
+++ server.ts
@@ -7,9 +7,10 @@ import {prod} from './utils';
 import * as fs from 'somes/fs';
 import paths from './paths';
 import * as path from 'path';
-import service from 'somes/service';
+//import service from 'somes/service';
 import {Descriptors} from 'somes/http_service';
 import cfg_ from './cfg';
+import {ServerIMPL,Options} from 'somes/server';
 
 export const cfg = {
 	temp: `${paths.var}/temp`,
@@ -35,40 +36,46 @@ cfg.router.push({
 	service: 'files',
 });
 
-// register service test
-if (!prod) {
-	for (var e of cfg_.tests || []) {
-		var {name,dir} = path.parse(e);
-		service.set(name, require(e).default(name, dir));
-		cfg.router.push({ match: `/${name}/{test_name}`, service: name, action: 'index' });
-	}
-	// service.set('test-ws', require('./test/test-ws').default);
-}
+export class Server extends ServerIMPL {
+	constructor(config?: Options) {
+		super(Object.assign({}, config, cfg));
 
-service.del('descriptors'); // delete descriptors service
-service.set('descriptors', class extends Descriptors {
-	descriptors() {
-		// this.markReturnInvalid();
-		this.setNoCache();
-		this.returnErrorStatus(404);
-		return {};
-	}
-});
+		// register service test
+		if (cfg_.env == 'dev' || cfg_.env == 'rel') {
+		for (var e of cfg_.tests || []) {
+			var {name,dir} = path.parse(e);
+			this.setService(name, require(e).default(name, dir));
+			cfg.router.push({ match: `/${name}/{test_name}`, service: name, action: 'index' });
+		}
+		// service.set('test-ws', require('./test/test-ws').default);
+		}
 
-export function initializeApi() {
-	// register service
-	cfg_.apis.forEach(dir=>{
+		this.removeService('descriptors'); // delete descriptors service
+		this.setService('descriptors', class extends Descriptors {
+		descriptors() {
+			// this.markReturnInvalid();
+			this.setNoCache();
+			this.returnErrorStatus(404);
+			return {};
+		}
+		});
+	}
+
+	initializeApi() {
+		// register service
+		cfg_.apis.forEach(dir=>{
 		if (fs.existsSync(dir)) {
 			fs.readdirSync(dir).forEach((e)=> {
-				var {name,ext} = path.parse(e);
-				if (ext == '.js' && name[0] != '_') {
-					if (fs.statSync(`${dir}/${e}`).isFile()) {
-						service.set(name, require(`${dir}/${name}`).default);
-					}
-				}
+			var {name,ext} = path.parse(e);
+			if (ext == '.js' && name[0] != '_') {
+				if (fs.statSync(`${dir}/${e}`).isFile())
+				this.setService(name, require(`${dir}/${name}`).default);
+			}
 			});
 		}
-	});
-	service.del('descriptors'); // delete descriptors service
-	service.set('descriptors', Descriptors); // add descriptors service
+		});
+		this.removeService('descriptors'); // delete descriptors service
+		this.setService('descriptors', Descriptors); // add descriptors service
+	}
+
 }
