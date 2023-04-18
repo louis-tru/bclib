3e1b90b08cf43700fbc09e634befbf8f2f8e7986
chuxuewen <louistru@hotmail.com>
2023-04-18 13:01:12 +0800

    update redis

--- redis.ts
+++ redis.ts
@@ -17,8 +17,8 @@ export class Redis {
 	readonly client: RedisClientType;
 	private _connected = false;
 
-	constructor(url: string, pwd?: string) {
-		this.client = createClient({url, password: pwd||undefined});
+	constructor(url: string) {
+		this.client = createClient({url});
 	}
 
 	async get<T = any>(key: string) {
@@ -88,6 +88,6 @@ export async function test() {
 	console.log(b_a, b2);
 }
 
-const _default = new Redis(cfg.redis || 'redis://127.0.0.1:6379/0', cfg.redis_pwd);
+const _default = new Redis(cfg.redis || 'redis://127.0.0.1:6379/0');
 
 export default _default;
