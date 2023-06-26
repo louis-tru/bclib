c24e96febd888733b6bc1dd8aee51b4cdd8c3367
chuxuewen <louistru@hotmail.com>
2023-06-26 13:36:31 +0800

    update deps amd env

--- env.ts
+++ env.ts
@@ -11,10 +11,10 @@ export const workers = Number(process.env.__WORKERS) && process.env.__WORKER ? {
 	workers: Number(process.env.__WORKERS) || 0,
 }: null;
 
-export const disableWeb = !!process.env.DISABLE_WEB;
+export const disableWeb = !!Number(process.env.DISABLE_WEB);
 
 export const env: 'prod' | 'dev' | 'rel' = cfg.env as any;
 
-export const tx_dequeue = cfg.web3_tx_dequeue || !!process.env.WEB3_TX_DEQUEUE;
+export const tx_dequeue = cfg.web3_tx_dequeue || !!Number(process.env.WEB3_TX_DEQUEUE);
 
 export const type = process.env.PROC_TYPE || cfg.type;
