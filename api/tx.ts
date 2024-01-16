d9b0acffa12f4735d78671ac39c576ecbeb54388
chuxuewen <louistru@hotmail.com>
2024-01-16 16:24:30 +0800

    updata: tx

--- api/tx.ts
+++ api/tx.ts
@@ -65,7 +65,9 @@ export default class extends APIController {
 		return await web3[chain].tx.post(address, method, args, opts, callback, noTryCall);
 	}
 
-	async deploy({chain,bytecode,abi,args,callback,...opts}: {chain: number, bytecode: string, abi: AbiItem[], args?: any[], callback?: string} & Options) {
+	async deploy({chain,bytecode,abi,args,callback,...opts}: {
+		chain: number, bytecode: string, abi: AbiItem[], args?: any[], callback?: string } & Options) 
+	{
 		await keys.impl.checkPermission(this.userName, opts.from);
 		return await web3[chain].tx.deploy(bytecode, abi, args, opts, callback);
 	}
